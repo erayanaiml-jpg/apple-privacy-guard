ba0ab571fdbda34af541506f3270934f76e8caa7
erayanaiml-jpg <er.ayanaiml@gmail.com>
2026-06-11 14:31:45 +0530

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,4 @@
+// Apple AI Privacy-Guard: Popup Controller
+document.addEventListener('DOMContentLoaded', function () {
+    console.log(" Privacy-Guard Popup UI successfully loaded and initialized.");
+});
